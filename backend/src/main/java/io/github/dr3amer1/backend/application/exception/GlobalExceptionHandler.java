19a05b94809d5ea8fc26ff5db8d98933f79e2a0b
Stepan <stepanmalygin0@gmail.com>
2026-06-06 19:08:18 +0300

    Добавлен REST API для сущности книг согласно CRUD, DTO для передачи информации между класами а также mapper для конвертации запросов в сущности и обратно.

---
+++ backend/src/main/java/io/github/dr3amer1/backend/application/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package io.github.dr3amer1.backend.application.exception;
+
+import io.github.dr3amer1.backend.presentation.exception.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> handleNotFound(
+            EntityNotFoundException ex) {
+
+        return Map.of(
+                "message",
+                ex.getMessage()
+        );
+    }
+}
---
+++ backend/src/main/java/io/github/dr3amer1/backend/presentation/dto/BookResponse.java
@@ -0,0 +1,25 @@
+package io.github.dr3amer1.backend.presentation.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+public class BookResponse {
+
+    private Long id;
+
+    private String title;
+
+    private String author;
+
+    private String description;
+
+    private BigDecimal price;
+
+    private String previewUrl;
+
+    private Integer publicationYear;
+}
