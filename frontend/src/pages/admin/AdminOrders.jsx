47d71f3ca88e5006215af5628f12b7aba43483b8
Asya <lunachara22@gmail.com>
2026-06-09 09:44:34 +0300

    Добавление функций для админа

--- frontend/src/pages/admin/AdminOrders.jsx
+++ frontend/src/pages/admin/AdminOrders.jsx
@@ -1,9 +1,111 @@
+import { useEffect, useState } from "react";
+
+import {
+    getOrders
+} from "../../api/ordersApi";
+
+
 function AdminOrders() {
+
+    const [orders, setOrders] = useState([]);
+
+    const loadOrders = () => {
+        getOrders()
+            .then(response => {
+                setOrders(response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    };
+
+
+    useEffect(() => {
+        loadOrders();
+    }, []);
+
+
     return (
-        <h1>
-            Управление заказами
-        </h1>
+        <div className="admin-page">
+
+            <h1>
+                Управление заказами
+            </h1>
+
+            <div className="admin-list">
+
+                {
+                    orders.map(order => (
+
+                        <div
+                            className="admin-card"
+                            key={order.id}
+                        >
+
+                            <div className="admin-card-title">
+                                Заказ №{order.id}
+                            </div>
+
+                            <div className="admin-card-text">
+
+                                Пользователь: {
+                                order.user
+                                    ?
+                                    order.user.username
+                                    :
+                                    "Неизвестно"
+                            }
+
+                                <br />
+
+                                Дата: {order.createdAt}
+
+                                <br />
+
+                                Статус: {order.status}
+
+                            </div>
+
+
+                            {
+                                order.items &&
+
+                                <div className="admin-card-text">
+
+                                    <b>
+                                        Товары:
+                                    </b>
+
+                                    {
+                                        order.items.map(item => (
+
+                                            <p key={item.id}>
+
+                                                {item.book?.title}
+
+                                                {" × "}
+
+                                                {item.quantity}
+
+                                            </p>
+
+                                        ))
+                                    }
+
+                                </div>
+                            }
+
+                        </div>
+
+                    ))
+                }
+
+            </div>
+
+        </div>
     );
 
 }
+
+
 export default AdminOrders;
